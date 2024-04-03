8b42a46a35c040ba863713a8c50aeb4a7f23e7ea
Arief Mahendra <ariefmahendra985@gmail.com>
2024-04-03 17:15:55 +0700

    feat: add form feature

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [
+    NgOptimizedImage,
+    FormsModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   username: string = "ariefmahendra";
   logoUrl: string = "/assets/angular-icon.svg";
-  logoAlt: string = "Logo"
+  logoAlt: string = "Logo";
+
+  favoriteFramework: string = '';
 }
